7ca96311b53db569ef39cbf92529309a4f05b503
cndn <373515162@qq.com>
2017-10-10 13:05:33 -0400

    word count

--- statistics/word_count.py
+++ statistics/word_count.py
@@ -16,29 +16,6 @@ def word_count(train):
             try:
                 with open(code_path, 'r') as code_f:
                     code = code_f.read()
-                    words = tokenizer.tokenize_code_without_id(code) + ['<eos>']
-                    for word in words:
-                        d[word] = d.get(word, 0) + 1
-            except:
-                pass
-    for item in sorted(d.items(), key=operator.itemgetter(1), reverse=True):
-        if len(item[0]) > 10:
-            continue
-        output.write(item[0] + '\t' + str(item[1]) + '\n')
-
-def naive_word_count(train):
-    d = {}
-    output = open('naive_word_count.txt','w')
-    with open(train,'r') as f:
-        for i, line in enumerate(f):
-            if i % 1000 == 0:
-                print i
-            filename = line.strip()
-            code_path = '../raw_data/' + filename
-            assert os.path.exists(code_path)
-            try:
-                with open(code_path, 'r') as code_f:
-                    code = code_f.read()
                     words = tokenizer.tokenize_wrapper(code) + ['<eos>']
                     for word in words:
                         d[word] = d.get(word, 0) + 1
