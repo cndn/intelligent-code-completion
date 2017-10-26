1add2ef8c2e4f5e7a66c2e3c06f41814d78fb8a7
cndn <373515162@qq.com>
2017-10-25 21:01:00 -0400

    tokenizer

--- tokenizer/tokenizer.py
+++ tokenizer/tokenizer.py
@@ -3,21 +3,29 @@
 
 from parso.python.tokenize import tokenize
 
+index = 0
 def tokenize_code(code):
     return [(token.type, token.string if token.type != 3 else '') for token in tokenize(code, (2, 7))]
 
-def tokenize_code_no_rare(code, vocab=set()):
+def generate_pseudo_words(w, pseudo_words,p_dict):
+    global index
+    if w in p_dict:
+        return 'pvar_'+str(p_dict[w])
+    index = (index + 1) % 10
+    p_dict[w] = index
+    return 'pvar_'+str(pseudo_words[index])
+
+def tokenize_code_no_rare(code, **kwargs):
     res = tokenize_code(code)
-    if len(vocab) == 0:
-        return res
-    return [a for a in res if str(a) in vocab]
+    vocab, pseudo_words, p_dict = kwargs.get('vocab',set()), kwargs['pseudo_words'], kwargs['p_dict']
+    return [a if str(a) in vocab else (a[0],generate_pseudo_words(a[1],pseudo_words,p_dict)) for a in res]
 
 def naive_tokenizer(string):
     return [val if val in wordFreq else 'unk' for val in re.findall('\\w+', string)]
 
-def tokenize_wrapper(string, vocab = set()):
-    return tokenize_code_no_rare(string, vocab)
+def tokenize_wrapper(string, **kwargs):
+    return tokenize_code_no_rare(string, **kwargs)
 
 if __name__ == '__main__':
     f = open('../dataset/split.py','r')
-    print tokenize_code(f.read())
+    print tokenize_wrapper(f.read())
